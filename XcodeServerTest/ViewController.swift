095dece1993e2f229185e2dbb028e6015eefb6f2
Piotr Perzanowski <piotr.perzanowski@it-huset.se>
2017-12-14 18:28:58 +0100

    Initial commit.

---
+++ XcodeServerTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeServerTest
+//
+//  Created by Piotr Perzanowski on 2017-12-14.
+//  Copyright © 2017 Piotr Perzanowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
